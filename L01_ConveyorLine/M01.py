e77dea7a190e24965a73a77b236207b5e74e3427
chanheegit <kimchan011227@gmail.com>
2025-03-10 18:04:10 +0900

    fianl commit

--- L01_ConveyorLine/M01.py
+++ L01_ConveyorLine/M01.py
@@ -17,21 +17,20 @@ pwm = GPIO.PWM(ENA, 1000)  # PWM 핀, 주파수 1kHz 설정
 pwm.start(0)  # PWM 초기값 0%
 
 # 모터 속도 설정 (0 ~ 100)
-speed = 20
+speed = 23
 
 try:
     while True:
-        # 모터 회전 설정 및 속도 유지
+        # 모터 회전 설정
         GPIO.output(IN1, GPIO.HIGH)
         GPIO.output(IN2, GPIO.LOW)
         pwm.ChangeDutyCycle(speed)
-
-        time.sleep(0.1)  # 주기적으로 상태 확인
+        time.sleep(0.1)  # 잠시 대기
 
 except KeyboardInterrupt:
     # 사용자가 Ctrl+C를 눌렀을 때 실행
     print("모터 중지")
-
+    
 finally:
     # 프로그램 종료 시 모터와 GPIO 설정 정리
     pwm.stop()
